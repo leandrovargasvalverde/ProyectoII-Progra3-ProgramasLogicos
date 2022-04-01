ef2ae3498b023a7f0ec74642b2de3e06cab8f788
leandrovargasvalverde <leandro9700@gmail.com>
2022-04-01 13:29:11 -0600

    UpdateEjercicios

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 2.Un algoritmo que invierta los valores de un entero de 2 digitos, por ejemplo si recibi 45 se debe imprimir 54. */
+
+namespace Ejercicio2.NumeroInvertido
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ingresoNumero;
+            int Num, Decimal, Uni, Operacion;
+            Console.Write("\n Digite un numero de 2 digitos: ");
+            ingresoNumero = Console.ReadLine();
+            Num = int.Parse(ingresoNumero);
+            Decimal = Num / 10;
+            Uni = Num % 10;
+            Operacion = (Uni * 10) + Decimal;
+            Console.WriteLine("\n El numero que digito invertido es: " + Operacion);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 3.El usuario debe ingresar dos números y el programa mostrará el resultado de la operación (a+b)*(a-b). */
+
+namespace Ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ingresoNumero;
+            int Num1,Num2, Suma, Resta, Multiplicacion;
+            Console.Write("\n Digite el primer numero: ");
+            ingresoNumero = Console.ReadLine();
+            Num1 = int.Parse(ingresoNumero);
+            Console.Write("\n Digite el segundo numero: ");
+            ingresoNumero = Console.ReadLine();
+            Num2 = int.Parse(ingresoNumero);
+            Suma = Num1 + Num2;
+            Resta = Num1 - Num2;
+            Multiplicacion = Suma * Resta;
+            Console.WriteLine("\n Suma: " + Suma);
+            Console.WriteLine("\n Resta: " + Resta);
+            Console.WriteLine("\n El resultado el resultado de la operación (a+b)*(a-b) es: " + Multiplicacion);
+            Console.ReadKey();
+        }
+    }
+}
